9a5e0cb454cea16b5745ef7fd904abb4aa0cf825
Wrik Das <wrikdas22@gmail.com>
2022-03-12 13:05:48 +0530

    Switched to size from pixelsize for font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SauceCodePro Nerd Font Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "SauceCodePro Nerd Font Mono:size=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 /*  "FiraMono:pixelsize=15:antialias=true:autohint=true", */
